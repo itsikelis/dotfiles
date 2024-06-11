7b9af01b706854ef8d9cf756b01466f2b3e96f57
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2024-06-11 22:01:34 +0300

    Minor restructure

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,31 @@
 require("itsikelis.options")
 require("itsikelis.keymaps")
-require("itsikelis.plugins")
+
+require("plugins.launch")
+
+-- LSP
+spec("plugins.schemastore")
+spec("plugins.cmp")
+spec("plugins.mason")
+spec("plugins.lspconfig")
+spec("plugins.null-ls")
+
+-- Core
+spec("plugins.treesitter")
+spec("plugins.colorscheme")
+
+spec("plugins.nvim-tree")
+spec("plugins.bufferline")
+spec("plugins.lualine")
+
+spec("plugins.telescope")
+spec("plugins.which-key")
+
+-- Extras
+spec("plugins.autopairs")
+spec("plugins.rainbow-delimiters")
+spec("plugins.comment")
+spec("plugins.gitsigns")
+spec("plugins.undotree")
+
+require('plugins.lazy')
