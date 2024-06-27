747a512bdb80cf54eeb649c3d63ef13412d3dc5a
Ioanis Tsikelis <tsikelis.i@protonmail.com>
2024-06-27 12:54:35 +0300

    Fix gitsigns after update

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -36,21 +36,11 @@ function M.config()
 
 	require("gitsigns").setup({
 		signs = {
-			add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-			change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-			delete = { hl = "GitSignsDelete", text = "󰐊", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-			topdelete = {
-				hl = "GitSignsDelete",
-				text = "󰐊",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			changedelete = {
-				hl = "GitSignsChange",
-				text = "▎",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
+			add = { text = "▎" },
+			change = { text = "▎" },
+			delete = { text = "󰐊" },
+			topdelete = { text = "󰐊" },
+			changedelete = { text = "▎" },
 		},
 		signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 		numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -68,9 +58,7 @@ function M.config()
 			delay = 50,
 			ignore_whitespace = false,
 		},
-		current_line_blame_formatter_opts = {
-			relative_time = false,
-		},
+		current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
 		sign_priority = 6,
 		update_debounce = 100,
 		status_formatter = nil, -- Use default
@@ -82,9 +70,6 @@ function M.config()
 			relative = "cursor",
 			row = 0,
 			col = 1,
-		},
-		yadm = {
-			enable = false,
 		},
 	})
 end
