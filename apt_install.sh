e3f027d58ee76755c4625cf5da2c4bfdf6ae748f
Ioannis Tsikelis <--global>
2025-11-28 15:53:30 +0100

    Update apt_install

--- apt_install.sh
+++ apt_install.sh
@@ -6,7 +6,13 @@ sudo apt -y install build-essential
 sudo apt -y install cmake
 sudo apt -y install ninja-build
 
-cd
-git clone https://github.com/neovim/neovim
-cd neovim && make CMAKE_BUILD_TYPE=Release
+git clone https://github.com/neovim/neovim ~/neovim
+cd ~/neovim && make CMAKE_BUILD_TYPE=Release
 sudo make install
+
+sudo apt -y install clang-format black luarocks ripgrep
+
+sudo apt -y install zsh
+
+sudo apt -y install tmux
+
